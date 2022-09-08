1beec162014bba93b91d63a7ad08457f4487878f
root <2603367658@qq.com>
2022-09-08 15:19:15 +0800

    leecode_1

--- c++1.cpp
+++ c++1.cpp
@@ -1,7 +1,26 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
 int main()
 {
-    std::cout<<"巴嘎"<<std::endl;
-    return 0;
+    std::vector<int> v = {2,  5, 11, 15,7};
+    std::map<int, int> a;
+   
+    for (int i = 0; i < v.size(); ++i)
+    {
+        a.insert(std::make_pair(v[i], i));
+    }
+    //  for(auto c:a)
+    // {
+    //     std::cout<<c.first<<"  "<<c.second<<std::endl;
+    // }
+    for (int i = 0; i < v.size(); ++i)
+    {
+        if (a.count(9 - v[i]) > 0 && (a[9 - v[i]] != i))
+        {
+            std::cout << a[v[i]] << "  " << a[9 - v[i]] << std::endl;
+            break;
+        }
+    }
 }
