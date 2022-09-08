346310376e7afc61089edce94785839563f761c5
root <2603367658@qq.com>
2022-09-08 15:11:12 +0800

    删除的文件

--- git_1/build/CMakeFiles/my_git_cmake.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for my_git_cmake.
-# This may be replaced when dependencies are built.
--- git_1/c++1.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include<string>
-int main()
-{
-    std::cout<<"巴嘎"<<std::endl;
-    return 0;
-}
